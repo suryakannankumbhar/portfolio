0f02f686820ed136f64aebb630d7058aff7832cb
Surya Kannan Kumbhar <suryakannankumbhar@gmail.com>
2021-06-23 10:38:00 +0530

    fixed dark mode bugs

--- components/app.js
+++ components/app.js
@@ -1,9 +1,45 @@
+
 const chk = document.getElementById('chk');
 var theme = document.getElementById('myStyleSheet');
-chk.addEventListener('change', () => {
-    if (chk.checked) {
-        theme.setAttribute('href', '/components/dark.css');
-    } else {
-        theme.setAttribute('href', '/components/light.css');
+let sheet;
+var defaultTheme = '/components/light.css';
+var checkToggle = 'unchecked';
+var setStatus = localStorage.status;
+var setTheme = localStorage.theme;
+localStorage.setItem('status',checkToggle);
+
+
+// setting theme from local storage
+if (setTheme=="/components/dark.css"){
+        theme.href = setTheme;
+        setTheme='';
+        chk.checked= true;
+    }
+else{
+        theme.href = setTheme;
+        setTheme='';
+        chk.checked = false;
+    }
+    
+
+// setting theme from toggle button
+chk.addEventListener('change',()=>{
+    if(chk.checked){
+        sheet = '/components/dark.css';
+    }else{
+        sheet = '/components/light.css';
+
+    }
+    swapStyle(sheet)
+    
+    
+    function swapStyle(sheet) {
+        if (sheet == "/components/dark.css"){
+            theme.href = sheet;
+        }
+        else{
+            theme.href = sheet;
+        }
+        localStorage.setItem('theme',sheet)
     }
 });
--- components/projects.js
+++
@@ -1,9 +0,0 @@
-const chk = document.getElementById('chk');
-var theme = document.getElementById('myStyleSheet');
-chk.addEventListener('change', () => {
-    if (chk.checked) {
-        theme.setAttribute('href', '/components/dark.css');
-    } else {
-        theme.setAttribute('href', '/components/light.css');
-    }
-});
